2fff6a788442eb656cbafa58126d2eb320f3f173
yohobojo <47697720+yohobojo@users.noreply.github.com>
2019-02-20 20:26:57 -0700

    Nav Drawer works now

---
+++ app/src/main/java/com/example/rlwiki/HomeFragment.java
@@ -0,0 +1,16 @@
+package com.example.rlwiki;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/rlwiki/MainActivity.java
+++ app/src/main/java/com/example/rlwiki/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.rlwiki;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,8 +9,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.*;
-import android.content.*;
+import android.widget.Toast;
+
+import com.example.rlwiki.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -25,11 +23,24 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        }
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -73,25 +84,34 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-
         if (id == R.id.nav_cars) {
-            Intent i = new Intent(getApplicationContext(), CarActivity.class);
-            startActivity(i);
+            //Display Toast message
+            Toast.makeText(this, "Cars item clicked", Toast.LENGTH_SHORT).show();
 
-
-        } else if (id == R.id.nav_boosts) {
-
-        } else if (id == R.id.nav_wheels) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CarFragment()).addToBackStack(null).commit();
 
         }
+        else if (id == R.id.nav_boosts) {
+            //Display Toast message
+            Toast.makeText(this, "Boosts item clicked", Toast.LENGTH_SHORT).show();
 
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new BoostFragment()).addToBackStack(null).commit();
+        }
+        else if (id == R.id.nav_wheels) {
+            //Display Toast message
+            Toast.makeText(this, "Wheels item clicked", Toast.LENGTH_SHORT).show();
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new WheelFragment()).addToBackStack(null).commit();
+        }
+//      else if (id == R.id.nav_manage) {
+//
+//      }
+//      else if (id == R.id.nav_share) {
+//
+//      }
+//      else if (id == R.id.nav_send) {
+//
+//      }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
